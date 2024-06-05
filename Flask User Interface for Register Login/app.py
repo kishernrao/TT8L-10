2c20e1ab6b6c6bba4ff752de4ac9e666068c8c7a
Sivasubramaniam <154749508+revoristic@users.noreply.github.com>
2024-06-05 16:00:14 +0800

    Update app.py Added Database for Forums(Student and Lecturer mock page, changed to Forum) added comments

--- Flask User Interface for Register Login/app.py
+++ Flask User Interface for Register Login/app.py
@@ -22,7 +22,7 @@ login_manager.login_view = 'login'
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-db = SQLAlchemy(app)
+db = SQLAlchemy(app) # Creates all tables from db.Model
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key= True)
@@ -47,13 +47,13 @@ class Comment(db.Model):
     date_posted = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
     
 
-class QuestionForm(FlaskForm):
+class QuestionForm(FlaskForm): #Used FlaskForm to create the text box for Question
     email = StringField('Email', validators=[DataRequired(), Email()])
     question = StringField('Question', validators=[DataRequired()])
     description = TextAreaField('Description', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
-class CommentForm(FlaskForm):
+class CommentForm(FlaskForm): #Used FlaskForm to create the text box for Comment
     email = StringField('Email', validators=[DataRequired(), Email()])
     comment = TextAreaField('Comment', validators=[DataRequired()])
     submit = SubmitField('Submit')
@@ -72,23 +72,23 @@ def register():
         password = request.form.get('psw')
         password_reconfirm = request.form.get('psw-reconfirm')
         
-        email_split = email.split('@')
+        email_split = email.split('@') #split email to get domain
         domain = email_split[1]
         if domain!="mmu.edu.my" and domain!="student.mmu.edu.my":
             message = 'Not MMU Email'
             return render_template('register.html',message=message)
         
-        if password != password_reconfirm:
+        if password != password_reconfirm: #password and password reconfirm must be same
             message = 'Passwords do not match!'
             return render_template('register.html', message=message)
 
-        existing_user_email = User.query.filter_by(email=email).first()
+        existing_user_email = User.query.filter_by(email=email).first() # If user is already register, message is displayed
         existing_user_username = User.query.filter_by(username=username).first()
         if existing_user_email or existing_user_username:
             message = 'Username or email already exists!'
             return render_template('register.html', message=message)
 
-        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')#make the password hasshed
         if(domain=="mmu.edu.my"):
             new_user = User(email=email, username=username, password=hashed_password, is_lecturer=True)
         else:
@@ -97,7 +97,7 @@ def register():
         db.session.add(new_user)
         db.session.commit()
 
-        return redirect(url_for('login'))
+        return redirect(url_for('login'))#redirect from register to login page
 
     return render_template('register.html', message=message)
 
@@ -160,9 +160,9 @@ def login():
         if user and bcrypt.check_password_hash(user.password, password):
             login_user(user)
             if user.is_lecturer==True:
-                return redirect(url_for('lecturerforum')) #will fill in this later with my friend's html link
+                return redirect(url_for('lecturerforum')) #redirect to lecturerforum
             else :
-                return redirect(url_for('studentforum')) #will fill in this later with my friend's html link
+                return redirect(url_for('studentforum')) #redirect to studentforum
         else:
             message = 'Invalid username/email or password!'
             return render_template('login.html', message=message)
