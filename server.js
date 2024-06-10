3156e1a914d01d5d7b3864526bef2f167e4af5d7
Kishern <kishernrao@gmail.com>
2024-06-10 13:00:16 +0800

    added node server and quiz creation system

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.post('/add-question', (req, res) => {
+  const { question, options } = req.body;
+  const newQuestion = `${question}, ${options}\n`;
+
+  fs.appendFile(path.join(__dirname, 'public', 'questions.txt'), newQuestion, (err) => {
+    if (err) {
+      console.error('Failed to add question:', err);
+      res.status(500).send('Failed to add question.');
+    } else {
+      res.status(200).send('Question added successfully!');
+    }
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+
